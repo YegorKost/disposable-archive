f835e49ec12233be6bb224351a2073c2f60a0b8f
Iegor Kost <Iegor Kost@localhost>
2015-09-05 18:17:25 +0300

    Fixed test class LibZip4jTest

--- src/main/java/typesOfArchive/LibZip4j.java
+++ src/main/java/typesOfArchive/LibZip4j.java
@@ -1,8 +1,15 @@
 package typesOfArchive;
 
+import actionWithArchive.AllFiles;
 import interfases.TypeOfArchive;
-
 import java.io.File;
+import net.lingala.zip4j.core.*;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * A class that implements interface TypeOfArchive and uses <i>net.lingala.zip4j</i> library.
@@ -19,7 +26,29 @@ public class LibZip4j implements TypeOfArchive {
      */
     @Override
     public void packArchive(File file, File pathToArchive, String psw) {
+        // List that store archived files
+        ArrayList<File> filesToArchive;
 
+        try {
+            // Initiate ZipFile object with the File object
+            ZipFile zipFile = new ZipFile(pathToArchive);
+            // Add files to the list
+            filesToArchive = new AllFiles().getFiles(file);
+            // Initiate zip parameters
+            ZipParameters zipParameters = new ZipParameters();
+            zipParameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+            zipParameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+            // Set the encryption flag to true
+            zipParameters.setEncryptFiles(true);
+            zipParameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+            zipParameters.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+            zipParameters.setPassword(psw.toCharArray());
+            // Add files to the zip file
+            zipFile.addFiles(filesToArchive, zipParameters);
+
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -29,6 +58,19 @@ public class LibZip4j implements TypeOfArchive {
      */
     @Override
     public void unpackArchive(File file, File pathToFile, String psw) {
+
+        try {
+            // Initiate ZipFile object with the File object
+            ZipFile zipFile = new ZipFile(file);
+            if (zipFile.isEncrypted()){
+                zipFile.setPassword(psw.toCharArray());
+            }
+            // Extract all files to define destination
+            zipFile.extractAll(pathToFile.getCanonicalPath());
+
+        } catch (ZipException | IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
